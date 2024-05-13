3ce3774279f4b70fad1544957493d225964c2585
Tyler Tierney <tytierney@yahoo.com>
2024-05-13 09:54:56 -0400

    use correct colors for theme icons

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import styles from "./Navbar.module.css";
 
 const sunIcon = (
   <svg
-    fill="var(--text-color)"
+    fill="inherit"
     height="2rem"
     width="2rem"
     version="1.1"
@@ -42,13 +42,14 @@ const moonIcon = (
   <svg
     width="2rem"
     height="2rem"
+    stroke="none"
     viewBox="0 0 24 24"
-    fill="none"
+    fill="inherit"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
       d="M12 22C17.5228 22 22 17.5228 22 12C22 11.5373 21.3065 11.4608 21.0672 11.8568C19.9289 13.7406 17.8615 15 15.5 15C11.9101 15 9 12.0899 9 8.5C9 6.13845 10.2594 4.07105 12.1432 2.93276C12.5392 2.69347 12.4627 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-      fill="#1C274C"
+      fill="inherit"
     />
   </svg>
 );
